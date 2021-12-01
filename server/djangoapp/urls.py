4cb068a7d1f38aeddf9c2b0e42c0b47bc0e9e7c7
verttisaurio1 <1daescalantevertti@gmail.com>
2021-12-01 05:37:09 +0000

    first changes to navbar and base

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,9 +10,9 @@ urlpatterns = [
     # name the URL
 
     # path for about view
-
+    path(route='about', view=views.about, name='about'),
     # path for contact us view
-
+    path(route='contact', view=views.contact, name='contact'),
     # path for registration
 
     # path for login
@@ -20,7 +20,7 @@ urlpatterns = [
     # path for logout
 
     path(route='', view=views.get_dealerships, name='index'),
-
+    path(route='index', view=views.get_dealerships, name='index'),
     # path for dealer reviews view
 
     # path for add a review view
